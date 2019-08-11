8a0cbd2dd669ea025524bf4053e5f5d29017f11e
v97ug <haskellscala@gmail.com>
2019-08-11 22:31:24 +0900

    [implement] Profileのセクション実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,22 +31,38 @@
       </div>
     </div>
 
-    <div class="container">
-      <h1>Profile</h1>
-      <img src="#">
-      <p>本名：宮城毅之
-        6歳から15歳までクラシックのピアノを習う。大学の頃に親友にアニメを勧められ、そのアニメのオープニングに流れていた曲に心を惹かれ、
-        アニソンの世界に没頭し始める。
-        以来、アニソンをピアノで弾くライブを開催したりYouTubeに動画投稿したりするなど、聴く人を心から感動させるピアノを弾いている。
-      </p>
+    <div class="profile-container">
+      <!--      <h1>Profile</h1>-->
+      <div class="text-center">
+        <img src="../assets/profile_header.png" alt="プロフィールのヘッダー" height="120">
+      </div>
 
+      <div class="profile-contents">
+        <div class="profile-img">
+          <img class="profile-img-border" src="../assets/profile_img2.png" alt="TAKECHANのプロフィール画像" width="300">
+        </div>
+        <div class="profile-text">
+          <div>名前：TAKECHAN(たけちゃん)</div>
+          <div>
+            6歳から15歳までクラシックのピアノを習う。大学の頃に親友にアニメを勧められ、そのアニメのオープニングに流れていた曲に心を惹かれ、
+            アニソンの世界に没頭し始める。
+            以来、アニソンをピアノで弾くライブを開催したりYouTubeに動画投稿したりするなど、聴く人を心から感動させるピアノを弾いている。
+          </div>
+          <div class="text-right">
+            >もっと見る
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="container">
       <a href="https://jp.freepik.com/free-photos-vectors/background">Freepik - jp.freepik.com によって作成された background
         ベクトル</a>
 
       <div>
         COPYRIGHT ©アニソンピアニスト TAKECHAN ALL RIGHT RESERVED.
       </div>
-    </div>
+    </footer>
   </div>
 </template>
 
@@ -115,5 +131,34 @@
   .button-container {
     margin: 30px auto 100px;
     text-align: center;
+  }
+
+  .profile-container {
+    width: 80%;
+    margin-top: 100px;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+    font-size: 22px;
+    background: #282828;
+  }
+
+  .profile-contents {
+    margin: 60px auto;
+    display: flex;
+  }
+
+  .profile-img-border {
+    border: 5px solid #571616;
+    box-shadow: 10px 10px 10px rgba(192, 156, 161, 0.3);
+  }
+
+  .profile-img {
+    border: 1px solid #535353;
+  }
+
+  .profile-text {
+    margin-left: 50px;
   }
 </style>
